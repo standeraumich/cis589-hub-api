88466adacc7a82549c5e7db0c54ca6cabfff1a74
standera <standera@umich.edu>
2024-04-24 21:03:58 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var birdsRouter = require('./routes/birds');
+var dotenv = require('dotenv')
+var cors = require('cors')
+var app = express();
+dotenv.config()
+
+app.use(cors({ origin: `http://${process.env.HOST}:3001` }));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/birds', birdsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+module.exports = app;
---
+++ routes/birds.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql2');
+var dotenv = require('dotenv');
+dotenv.config()
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise()
+
+async function getData() {
+  const [rows] = await pool.query("SELECT * FROM birds")
+  return rows
+}
+
+async function getBirdLatest() {
+  const dataList = []
+  const [row] = await pool.query(`
+    SELECT a.*
+    FROM \`birds\` a
+	  LEFT JOIN \`birds\` b
+		ON a.bird = b.bird AND a.created < b.created
+    WHERE b.created is NULL;
+    `)
+  row.map((val, key) => (
+    dataList.push(val.created, val.bird, val.lat, val.lon)
+  ));
+  return dataList
+}
+
+async function insertBirdData(bird, lat, lon) {
+  const result = await pool.query(`
+    INSERT INTO birds (bird, lat, lon)
+    VALUES (?, ?, ?)
+    `, [bird, lat, lon])
+  return result
+}
+
+/* GET latest bird data. */
+router.get('/latest', async function (req, res, next) {
+  const birdData = await getBirdLatest()
+  // res.json({
+  //   data: birdData
+  // });
+  res.send(birdData)
+});
+
+/* GET all bird data. */
+router.get('/', async function (req, res, next) {
+  const birdData = await getData()
+  res.send(birdData)
+});
+
+/* POST bird data entry */
+router.post('/', async function (req, res, next) {
+  const { bird, lat, lon } = req.body
+  const birdData = await insertBirdData(bird, lat, lon)
+  res.status(201).send(birdData)
+});
+
+
+module.exports = router;
